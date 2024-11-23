3117d8f6c026e2657b37ea317d54e00fec91053c
Maohead <folzy312@gmail.com>
2024-11-23 22:01:40 +0900

    added image beers

--- app/src/main/java/com/example/appbeer/MainActivity.kt
+++ app/src/main/java/com/example/appbeer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.appbeer
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
@@ -11,6 +12,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,10 +26,18 @@ class MainActivity : AppCompatActivity() {
         val btnBeer : Button = findViewById(R.id.btnBeer)
         val tvBeer : TextView = findViewById(R.id.tvBeer)
         val spinnerBeer : Spinner = findViewById(R.id.spinBeer)
+        val imageBeer : ImageView = findViewById(R.id.imBeer)
 
         btnBeer.setOnClickListener {
             val type = spinnerBeer.selectedItem
             tvBeer.text = "Ваш тип пива $type"
+            when (spinnerBeer.selectedItem) {
+                "Тёмное" -> imageBeer.setImageResource(R.drawable.temnoe)
+                "Светлое" -> imageBeer.setImageResource(R.drawable.svetloe)
+                "Ржаное" -> imageBeer.setImageResource(R.drawable.rhanoe)
+                "Сладкое" ->imageBeer.setImageResource(R.drawable.sladkoe)
+                "Горькое" ->imageBeer.setImageResource(R.drawable.gorkoe)
+            }
         }
     }
 }
